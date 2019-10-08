488134aca61a52f0ef88465b9444e16121ece644
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-10-08 11:27:28 +0530

    Initial commit

---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const { app } = electron;
+const { BrowserWindow } = electron;
+
+let mainWindow;
+
+function createWindow() {
+  const startUrl = process.env.DEV
+    ? 'http://localhost:3000'
+    : url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true,
+      });
+  mainWindow = new BrowserWindow();
+  mainWindow.show();
+
+  mainWindow.loadURL(startUrl);
+  process.env.DEV && mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
